d1c712c139f91bbb8dafcaf7db70cd5b4b12c4d2
gaboe <gabrielecegi@gmail.com>
2018-05-18 23:35:56 +0200

    generate random array of length n

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <list>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,3 +13,18 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_nonScalableButton_clicked()
+{
+    ui->nonScalableList->clear();
+    std::list<int> list;
+    for (int i = 0; i < ui->nonScalableSpinBox->value(); i++) {
+        list.push_back(rand() % 32);
+    }
+    list.sort();
+
+    foreach (auto item, list) {
+        ui->nonScalableList->addItem(QString::fromStdString(std::to_string(item)));
+    }
+}
+
